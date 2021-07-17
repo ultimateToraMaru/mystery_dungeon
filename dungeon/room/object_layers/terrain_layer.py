aeb6e376924326d066848bb993f5c5de27bc15c5
ultimateToraMaru <60610126+ultimateToraMaru@users.noreply.github.com>
2021-07-17 21:17:59 +0900

    一応すべての部屋を経路でつなぐことができた。幅優先選択で経路作るかな？

--- dungeon/room/object_layers/terrain_layer.py
+++ dungeon/room/object_layers/terrain_layer.py
@@ -13,7 +13,7 @@ class Terrain_layer():
         if (type == 'normal'):
             self.__data = [[Wall(Color.RED)] * Properties.MAX_MASS_IN_ROOM_ONE_SIDE for i in range(Properties.MAX_MASS_IN_ROOM_ONE_SIDE)]      
             self.__data = self.generate(self.__data, Properties.MAX_MASS_IN_ROOM_ONE_SIDE)
-            self.__data = self.setExportPoints(self.__data, Properties.MAX_MASS_IN_ROOM_ONE_SIDE-1);
+            self.__data = self.setExportPoints(self.__data, Properties.MAX_MASS_IN_ROOM_ONE_SIDE);
         elif(type == 'none'):
             self.__data = [[Wall(Color.BROWN)] * Properties.MAX_MASS_IN_ROOM_ONE_SIDE for i in range(Properties.MAX_MASS_IN_ROOM_ONE_SIDE)]
             self.__data = self.generate_none_room(self.__data)
@@ -30,33 +30,28 @@ class Terrain_layer():
 
         # 部屋の端っこ。始まりポイントと終わりポイント
         start_point = 2
+        margin = 3
         end_point = MAXMASS-1
         
         # 四角形の4点をランダムで決める
-        r_start_x = random.randint(start_point, start_point+2) 
-        r_end_x = random.randint(r_start_x+start_point, end_point) 
-        r_start_y = random.randint(start_point, start_point+2) 
-        r_end_y = random.randint(r_start_y+start_point, end_point) 
+        r_start_x = random.randint(start_point, start_point+1) 
+        r_end_x = random.randint(r_start_x+margin, end_point) 
+        r_start_y = random.randint(start_point, start_point+1) 
+        r_end_y = random.randint(r_start_y+margin, end_point) 
 
-        # 出口ポイントをランダムで決める
-        r_exit_point_x_1 = random.randint(r_start_x+1, r_end_x-1)
-        r_exit_point_x_2 = random.randint(r_start_x+1, r_end_x-1)
-        r_exit_point_y_3 = random.randint(r_start_y+1, r_end_y-1)
-        r_exit_point_y_4 = random.randint(r_start_y+1, r_end_y-1)
-
-        print(r_exit_point_x_1, r_exit_point_x_2, r_exit_point_y_3, r_exit_point_y_4)
-
+        # # 出口ポイントをランダムで決めて、道をつくる
         # r = random.randint(1, 3)
-        # point_r = random.randint(0, MAXMASS-1)
 
-    # if (side_r == 0):
-        data[r_start_y-1][r_exit_point_x_1] = Tile(Color.GREEN)
-    # elif (side_r == 1):
-        data[r_end_y][r_exit_point_x_2] = Tile(Color.GREEN)
-    # elif (side_r == 2):
-        data[r_exit_point_y_3][r_start_x-1] = Tile(Color.GREEN)
-    # elif (side_r == 3):
-        data[r_exit_point_y_4][r_end_x] = Tile(Color.GREEN)
+        # center = math.floor(MAXMASS/2)
+        # for i in range(5):
+        #     if (r == 0):
+        #         data[0+i][center] = Tile(Color.GREEN)
+        #     elif (r == 1):
+        #         data[center+i][center] = Tile(Color.GREEN)
+        #     elif (r == 2):
+        #         data[center][0+i] = Tile(Color.GREEN)
+        #     elif (r == 3):
+        #         data[center][center+i] = Tile(Color.GREEN)
 
         # Y軸の端っこに床を配置していく
         for i in range(r_start_y, r_end_y):
@@ -93,20 +88,22 @@ class Terrain_layer():
     # 入り口出口は部屋が持つべきだよね。ってことで、地形データ(terrain_layer)に持ってきたのだ
     # 部屋間の道はその道が通る部屋が持つべきだよね。
     def setExportPoints(self, data, MAXMASS):
-        # r = random.randint(1, 3)
-        # for i in range(r):
-        #     side_r = random.randint(0, MAXMASS-1)
-        #     point_r = random.randint(0, MAXMASS-1)
 
-        #     if (side_r == 0):
-        #         data[1][point_r] = Tile(Color.GREEN)
-        #     elif (side_r == 1):
-        #         data[MAXMASS-1][point_r] = Tile(Color.GREEN)
-        #     elif (side_r == 2):
-        #         data[point_r][1] = Tile(Color.GREEN)
-        #     elif (side_r == 3):
-        #         data[point_r][MAXMASS-1] = Tile(Color.GREEN)
-        data
+        for i in range(random.randint(1, 4)):
+            # 出口ポイントをランダムで決めて、道をつくる
+            center = math.floor(MAXMASS/2)
+            # directions_r = random.randint(0, 3)
+            for i in range(5):
+                # if (directions_r == 0):
+                    data[0+i][center] = Tile(Color.GREEN)
+                # elif (directions_r == 1):
+                    data[center+i][center] = Tile(Color.GREEN)
+                # elif (directions_r == 2):
+                    data[center][0+i] = Tile(Color.GREEN)
+                # elif (directions_r == 3):
+                    data[center][center+i] = Tile(Color.GREEN)
+        
+        # フロアの端っこの部屋の場合
         
         return data
 
