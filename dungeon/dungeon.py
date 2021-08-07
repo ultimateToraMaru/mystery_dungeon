c5dd76ef924a066345bcb098dfd9de0b9ee7e4c7
ultimateToraMaru <60610126+ultimateToraMaru@users.noreply.github.com>
2021-08-07 20:07:09 +0900

    dungeonクラスの__now_floorフィールドがいらなそうだったので、消し去りました

--- dungeon/dungeon.py
+++ dungeon/dungeon.py
@@ -13,7 +13,7 @@ class Dungeon:
             self.__floors.append(Floor())
         
         self.__name = 'ほげダンジョン'
-        self.__now_floor_index: int = 0
+        self.__now_floor_index: int = -1
         self.__turn: int = 1
 
         # self.__now_floor = Floor()      # いらない。now_floor_indexがあれば、この変数はいらない。いつか変えよう。
@@ -27,47 +27,53 @@ class Dungeon:
     def floors(self):
         return self.__floors
     
+    # フロア到着時に一回だけ呼び出される
     def start_turn(self):
-        self.__now_floor = self.get_next_floor()
+        # self.__now_floor = self.get_next_floor()
+        self.__now_floor_index += 1
 
-        self.__now_floor.spawn_player()
-        self.__now_floor.spawn_steps()
+        self.__floors[self.__now_floor_index].spawn_player()
+        self.__floors[self.__now_floor_index].spawn_steps()
 
-        print(self.__now_floor_index, '階')
+        print(self.__now_floor_index-1, '階')
     
+    # ターンを進める
     def forward_turn(self):
-        self.__camera.target = self.__now_floor.get_player_room_arounds()
+        self.__camera.target = self.__floors[self.__now_floor_index].get_player_room_arounds()
         self.__camera.show()
 
         self.input_check()
 
-        if (self.__now_floor.is_player_on_steps() == True):
+        if (self.__floors[self.__now_floor_index].is_player_on_steps() == True):
             print('next')
             self.start_turn()
 
-        self.__now_floor.player_set_position()
+        self.__floors[self.__now_floor_index].player_set_position()
     
-    def get_next_floor(self):
-        self.__now_floor_index += 1
-        next_floor = self.__floors[self.__now_floor_index-1]
 
-        return next_floor
+    # def get_next_floor(self):
+    #     self.__now_floor_index += 1
+    #     next_floor = self.__floors[self.__now_floor_index-1]
+
+    #     return next_floor
     
+    # 入力されたキーをチェックする
+    # 現在は方向キーだけ実装済み
     def input_check(self):
         if pyxel.btnp(pyxel.KEY_D):
             self.__turn += 1
             print('ターン:', self.__turn)
-            self.__now_floor.player_move('right')
+            self.__floors[self.__now_floor_index].player_move('right')
         elif pyxel.btnp(pyxel.KEY_A):
             self.__turn += 1
             print('ターン:', self.__turn)
-            self.__now_floor.player_move('left')
+            self.__floors[self.__now_floor_index].player_move('left')
         elif pyxel.btnp(pyxel.KEY_W):
             self.__turn += 1
             print('ターン:', self.__turn)
-            self.__now_floor.player_move('up')
+            self.__floors[self.__now_floor_index].player_move('up')
         elif pyxel.btnp(pyxel.KEY_S):
             self.__turn += 1
             print('ターン:', self.__turn)
-            self.__now_floor.player_move('down')
+            self.__floors[self.__now_floor_index].player_move('down')
     
